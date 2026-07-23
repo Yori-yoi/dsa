8e1f0d7f16e7716db2076d091a19eec447c1f95d
Yori-yoi <soysaycey@gmail.com>
2026-07-23 21:13:00 +0530

    creation of a tree

---
+++ tree.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+	int data;
+	struct Node* left;
+	struct Node* right;
+	};
+struct Node* create_node(int data){
+	struct Node* ptr= (struct Node*)malloc(sizeof(struct Node));
+	ptr->left=NULL;
+	ptr->right=NULL;
+	ptr->data=data;
+	return ptr;
+	}
+int main(){
+	struct Node *p1, *p2, *p3;
+	p1=create_node(3);
+	p2=create_node(4);
+	p3=create_node(5);
+	p1->left=p2;
+	p2->right=p3;
+	printf("success");
+	return 0;
+}
